28277db05623ed169fdfd50049a11f280e5960d6
gianherrera_belcorp <gianherrera@belcorp.biz>
2024-06-17 16:37:27 -0500

    Modify: Modified selectors where hash were added

--- cypress/e2e/ThirdScenario.cy.js
+++ cypress/e2e/ThirdScenario.cy.js
@@ -10,10 +10,7 @@ describe('template spec', () => {
     cy.get('[data-testid="advice-request-list-thead-name"]',{ timeout: 10000 }).should('exist');
     cy.get('[data-testid="advice-request-list-td-name-0"]',{ timeout: 10000 }).click();
     cy.get('[href*="/household"]', { timeout: 10000 }).contains('Household').click();
-    cy.get('[class*="styles_householdWrapper__header__"]', { timeout: 10000 }).should('exist');
-
-
-    
+    cy.get('[class*="styles_householdWrapper__header__"]', { timeout: 10000 }).should('exist');    
     cy.get('.fa-trash', { timeout: 5000 }).then(($element)=>{
       if($element.length > 0){
         cy.get('.fa-trash', { timeout: 10000 }).last().click()
--- cypress/e2e/firstScenario.cy.js
+++ cypress/e2e/firstScenario.cy.js
@@ -8,18 +8,18 @@ describe('template spec', () => {
     cy.get('#password').type('QA.role123')
     cy.get('.c6a8ea2a6').click()
     cy.get('li[data-menu-id*="add_new_client"]', { timeout: 10000 }).first().click();
-    cy.get('.styles_newClientOptions__options__YK_tM button', { timeout: 10000 }).first().click();
+    cy.get('[class*="styles_newClientOptions__options__"] button', { timeout: 10000 }).first().click();
     cy.get('.next-button', { timeout: 10000 }).click();
     cy.get('#input-search', { timeout: 10000 }).type('Eleanora Schneider');
-    cy.get('.styles-module_searchClient__option__e6fRD', { timeout: 10000 }).click();
+    cy.get('[class*="styles-module_searchClient__option__"]', { timeout: 10000 }).click();
     cy.get('.next-button', { timeout: 10000 }).click();
     cy.get('input[data-testid="email"]').type(email);
     cy.get('input[data-testid="name"]').type('Gian');
     cy.get('input[data-testid="last_name"]').type('Herrera');
     cy.get('input[data-testid="mobile"]').type('3193219951');
     cy.get('.next-button', { timeout: 10000 }).click();
-    cy.get('.styles_householdSuccessView__successMessage__ARlV0', { timeout: 10000 }).should('exist');
-    cy.get('.styles_link__modActive__2xcl0', { timeout: 10000 }).should('exist');
+    cy.get('[class*="styles_householdSuccessView__successMessage__"]', { timeout: 10000 }).should('exist');
+    cy.get('[class*="styles_link__modActive__"]', { timeout: 10000 }).should('exist');
     cy.get('p', { timeout: 10000 }).contains(email).should('exist'); 
     Cypress.on('uncaught:exception', (err, runnable) => {
       // returning false here prevents Cypress from
